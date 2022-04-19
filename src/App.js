460bed4fe7d8a22f089ad2fc15c889bf89a55c73
katieagne <katieagne@gmail.com>
2022-04-18 19:53:57 -0700

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { fetchProviders, fetchProvider } from "./api";
 import "./App.scss";
+import BrowseProviders from "./pages/BrowseProviders/BrowseProviders";
+import ProviderProfile from "./pages/ProviderProfile/ProviderProfile";
 
 function App() {
   // Samples of API requests
   fetchProviders().then(console.log);
   fetchProvider("1").then(console.log);
 
-  return <div className="App">clean!</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<BrowseProviders />} />
+          <Route path=":providerId" element={<ProviderProfile />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
